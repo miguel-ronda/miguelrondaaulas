0ce1b651825c22a65ba9ec3499c28b13c05534ed
miguel-ronda <miguel_ronda@estudante.sesisenai.org.br>
2025-02-19 19:21:30 -0300

    .

---
+++ EXEtuplasunpackig.py
@@ -0,0 +1,7 @@
+vendas = ("miguel",'25/08/2020',"23/03/2008",2000,"estagiario")
+nome,datacont,datanas,salario,cargo = vendas
+print(nome)
+print(datacont)
+print(datanas)
+print(salario)
+print(cargo)
